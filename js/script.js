5ff24581253cff51f03a8ee0c82a5b7d7e402b55
jjh <wjdwlgus96@gmail.com>
2021-04-27 14:59:37 +0900

    스핀몬 추가, pdf 추가, 이메일복사 기능

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ function clickTag(obj){
         }
     }
 }
+
+function clickEmail(){
+    alert('복사되었습니다.');
+    var tempElem = document.createElement('textarea');
+    tempElem.value = 'wjdwlgus96@google.com';
+    document.body.appendChild(tempElem);
+
+    tempElem.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempElem);
+}
